ee5ff7a19b4ca98b079dcb37d0b448b261496194
Artur Todeschni Crestani <artodeschini@gmail.com>
2023-03-20 15:58:29 -0300

    criado o esqueleto do componente do jogo da velha

--- src/app/jogo-da-velha/jogo-da-velha.module.ts
+++ src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { JogoDaVelhaComponent } from './jogo-da-velha.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    JogoDaVelhaComponent
+  ],
   imports: [
     CommonModule
+  ],
+  // necessario pois nao trabalharei com rotas
+  // caso nao adicionado nao podera ser visualizado no app principal
+  exports: [
+    JogoDaVelhaComponent
   ]
 })
 export class JogoDaVelhaModule { }
